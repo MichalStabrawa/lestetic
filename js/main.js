4b07d72942d87b21c1bcbf9f6dc17a04b91f0c82
Michal Stabrawa <mistaaqua@gmail.com>
2019-11-30 22:04:54 +0100

    box hover

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
-$(document).ready(function(){
-    $('.your-class').slick({
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        arrows: false,
-        autoplay: true,
-        fade: true
-    });
+$(document).ready(function() {
+  $(".your-class").slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+    fade: true,
+  });
 
-    $('.hamburger').click(function(){
-        $('.navbar').slideToggle(500)
-        
-    })
+  $(".hamburger").click(function() {
+    $(".navbar").slideToggle(500);
+  });
 
-    $('.cards-item').mouseover(function(){
-        $(".cards-price").fadeIn().css('dispaly','flex');
-        $(".cards-title-content").fadeOut()
-      });
-      $('.cards-item').mouseout(function(){
-        $(".cards-price").fadeOut(); 
-      })
-})
+  $(".col-content").mouseenter(function() {
+    $(this).find(".cards-title-content")
+      .css("display", "none");
+    $(this).find(".cards-price")
+      .fadeIn(500);
+    $(this).find(".cards-content-next")
+      .fadeIn(600);
+  });
 
-console.log(window.location.pathname)
+  $(".col-content").mouseleave(function() {
+    $(this).find(".cards-title-content")
+      .fadeIn(500);
+    $(this).find(".cards-price")
+      .css("display","none");
+    $(this).find(".cards-content-next")
+      .css("display","none");
+  });
+});
+
+console.log(window.location.pathname);
